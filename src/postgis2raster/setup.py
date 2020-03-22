0e01243cc48ce47defe98bd24b71ab3bec40e083
sandeepgadhwal <sandeepgadhwal1@gmail.com>
2020-03-22 17:22:45 +0530

    fixed readme

--- src/postgis2raster/setup.py
+++ src/postgis2raster/setup.py
@@ -1,13 +1,19 @@
-from distutils.core import setup
-setup(
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+	
+setuptools.setup(
   name = 'postgis2raster',
-  packages = ['postgis2raster'],
   version = '0.1',
-  license='MIT',
+  license = 'MIT',
   description = 'This library helps user create raster files directly off postgis tables, rasterization and fishnet analysis.',
+  long_description = long_description,
+  long_description_content_type = "text/markdown",
   author = 'SANDEEP KUMAR',
   author_email = 'sandeepgadhwal1@gmail.com',
   url = 'https://urbantalks.in',
+  packages = setuptools.find_packages(),
   download_url = 'https://github.com/sandeepgadhwal/postgis2raster/archive/v_01.tar.gz',
   keywords = ['postgis', 'raster', 'fishnet'],
   install_requires=[
@@ -16,7 +22,7 @@ setup(
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',    
-    'Intended Audience :: GIS Analyst', 
+    'Intended Audience :: Science/Research', 
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3', 
@@ -24,4 +30,5 @@ setup(
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
   ],
+  python_requires='>=3.6',
 )
